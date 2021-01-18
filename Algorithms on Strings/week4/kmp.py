04d827b5cffd12ebf050059f9b90aa981e20ae6d
shivani-aradhya <65466500+shivani-aradhya@users.noreply.github.com>
2021-01-18 22:06:15 +0530

    commit

---
+++ Algorithms on Strings/week4/kmp.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def find_pattern(pattern, text):
+
+  result = []
+  return result
+
+
+if __name__ == '__main__':
+  pattern = sys.stdin.readline().strip()
+  text = sys.stdin.readline().strip()
+  result = find_pattern(pattern, text)
+  print(" ".join(map(str, result)))
